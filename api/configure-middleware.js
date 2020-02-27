b38d3c138177023bb4e722f48804860c3f558123
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-02-27 00:29:30 -0800

    heroku

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -1,13 +1,13 @@
-const express = require('express');
+// const express = require('express');
 
-const helmet = require('helmet')
+// const helmet = require('helmet')
 
-const cors = require('cors');
+// const cors = require('cors');
 
 
 
-module.exports = server => {
-    server.use(express.json());
-    server.use(helmet());
-    server.use(cors());
-}
+// module.exports = server => {
+//     server.use(express.json());
+//     server.use(helmet());
+//     server.use(cors());
+// }
