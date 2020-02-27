480e171d6813166ee9c33c7796882984915da079
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-02-27 00:29:30 -0800

    heroku

--- api/server.js
+++ api/server.js
@@ -1,13 +1,37 @@
+// const express = require('express');
 const express = require('express');
 
-const foodTruckRouter = require('./api-router.js');
+const helmet = require('helmet')
 
-const configureMiddleware = require('./configure-middleware.js');
+const cors = require('cors');
+// const router = require('express').Router();
+
+const dinerAuthRouter = require('../auth/diner-auth-router.js');
+const operatorAuthRouter = require('../auth/operator-auth-router.js');
+const loginAuthRouter = require('../auth/login-router.js');
 
 const server = express();
 
-configureMiddleware(server);
+server.use('api/auth/operator', operatorAuthRouter);
+server.use('api/auth/diner', dinerAuthRouter);
+server.use('api/auth/login', loginAuthRouter);
 
-server.use('/api', foodTruckRouter);
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.get('/', (req,res) => {
+    res.json({api: 'working'})
+});
+
+// const foodTruckRouter = require('./api-router.js');
+
+// const configureMiddleware = require('./configure-middleware.js');
+
+
+
+// configureMiddleware(server);
+
+// server.use('/api', foodTruckRouter);
 
 module.exports = server
