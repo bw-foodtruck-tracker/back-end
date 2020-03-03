b0f3914968b4e6a62cd8fc713dd800802578b909
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-03-02 16:36:15 -0800

    heroku

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,44 @@
-module.exports = {
 
+
+
+  module.exports = {
     development: {
-      client: 'sqlite3',
-      useNullAsDefault: true, // needed for sqlite
-      connection: {
-        filename: './database/foodTruck.db3',
-      },
-      migrations: {
-        directory: './database/migrations'
-      },
-      seeds: {
-        directory: './database/seeds'
-      },
-      // add the following
+      client: "sqlite3",
+      connection: { filename: './database/foodTruck.db3' },
+      useNullAsDefault: true,
       pool: {
         afterCreate: (conn, done) => {
-          // runs after a connection is made to the sqlite engine
-          conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-        },
+          conn.run("PRAGMA foreign_keys = ON", done);
+        }
       },
+      migrations: {
+        directory: "./database/migrations"
+      },
+      seeds: { directory: "./database/seeds" }
+    },
+    testing: {
+      client: "sqlite3",
+      connection: {
+        filename: "./database/test.db3"
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: "./database/migrations"
+      },
+      seeds: {
+        directory: "./database/seeds"
+      }
     },
     production: {
-      client: 'pg',
+      client: "pg",
       connection: process.env.DATABASE_URL,
       pool: {
         min: 2,
         max: 10
       },
       migrations: {
-        directory: './database/migrations'
+        directory: "./database/migrations"
       },
-      seeds: {
-        directory: './database/seeds'
-      }
-    } 
+      seeds: { directory: "./database/seeds" }
+    }
   };
