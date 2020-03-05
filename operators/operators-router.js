62f45a5a2ebc91a865aa6e5668d90898c3706bd0
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-03-04 19:57:19 -0800

    added geo for truck

--- operators/operators-router.js
+++ operators/operators-router.js
@@ -124,29 +124,34 @@ router.get('/:id/truck', restricted, checkRole(), (req,res) => {
 
 router.post('/:id/truck', restricted, checkRole(), validateOperatorId, validateTruckInfo, (req,res) => {
   
-  const newTruck = {
-    operator_id: req.params.id,
-    truckName: req.body.truckName,
-    imageOfTruck: req.body.imageOfTruck,
-    cuisineType: req.body.cuisineType,
-    currentLocation: req.body.currentLocation,
-    departureTime: req.body.departureTime,
-}
+  
     
     
         Operators.findByTruckName(req.body.truckName)
           .then(truck => {
-            if(truck.length > 0) {
-              res.status(400).json({ error: "Truck name must be unique" });
-            } else {
-              Operators.addTruck(newTruck)
-                .then(truck => {
-                    res.status(201).json(truck);
+            geocoder.geocode(req.body.currentLocation)
+              .then(geo => {
+                console.log(geo)
+                const newTruck = {
+                  operator_id: req.params.id,
+                  truckName: req.body.truckName,
+                  imageOfTruck: req.body.imageOfTruck,
+                  cuisineType: req.body.cuisineType,
+                  currentLocation: `${geo[0].latitude} ${geo[0].longitude}`,
+                  departureTime: req.body.departureTime,
+              }
+              if(truck.length > 0) {
+                res.status(400).json({ error: "Truck name must be unique" });
+              } else {
+                Operators.addTruck(newTruck)
+                  .then(truck => {
+                      res.status(201).json(truck);
+                  })
+                  .catch(err => {
+                      res.status(500).json({ error: "There was an error while saving the task to the database" });
                 })
-                .catch(err => {
-                    res.status(500).json({ error: "There was an error while saving the task to the database" });
-              })
-            }
+              }
+            })
           })
 })
 
@@ -155,28 +160,31 @@ router.put('/:id/truck', restricted, checkRole(), validateTruckId, validateTruck
  
     Diners.findByCustomerRatingTruckAvg(req.params.id)
         .then(avg => {
-          const updateTruck = {
-            truckName: req.body.truckName,
-            imageOfTruck: req.body.imageOfTruck,
-            cuisineType: req.body.cuisineType,
-            currentLocation: req.body.currentLocation,
-            departureTime: req.body.departureTime,
-            customerRatingAvg: Object.values(avg[0])[0]
-        }
-        Operators.findByTruckName(req.body.truckName)
-          .then(truck => {
-            if(truck.length > 0) {
-              res.status(400).json({ error: "Truck name must be unique" });
-            } else {
-              Operators.updateTruck(req.params.id, updateTruck)
-                .then(post => {
-                  res.status(200).json(post);
-                })
-                .catch(err => {
-                  res.status(500).json({error: "The truck information could not be modified"});
-                })
+          geocoder.geocode(req.body.currentLocation)
+            .then(geo=> {
+              const updateTruck = {
+                truckName: req.body.truckName,
+                imageOfTruck: req.body.imageOfTruck,
+                cuisineType: req.body.cuisineType,
+                currentLocation: `${geo[0].latitude} ${geo[0].longitude}`,
+                departureTime: req.body.departureTime,
+                customerRatingAvg: Object.values(avg[0])[0]
               }
-          })
+              Operators.findByTruckName(req.body.truckName)
+                .then(truck => {
+                  if(truck.length > 0) {
+                    res.status(400).json({ error: "Truck name must be unique" });
+                  } else {
+                    Operators.updateTruck(req.params.id, updateTruck)
+                      .then(post => {
+                        res.status(200).json(post);
+                      })
+                      .catch(err => {
+                        res.status(500).json({error: "The truck information could not be modified"});
+                      })
+                    }
+                })
+            })
         })   
   });
 
